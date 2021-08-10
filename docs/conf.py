16c3afcfa80c1e89ee6d6b4f31e41e9d34809622
Maria Wisniewska <maria.wisniewska@nxp.com>
2021-08-10 09:32:55 +0200

    Synch-up to release 1.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,6 @@ extensions = [
     "sphinx.ext.todo",
     "recommonmark",
     "sphinx_click",
-#     "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
